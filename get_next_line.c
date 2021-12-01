b3ddd1b1030c43d6993f5b30a3e157c14908e841
jaesjeon <jaesjeon@student.42seoul.kr>
2021-12-01 21:50:21 +0900

    gnl_211201

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: jaesjeon <jaesjeon@student.42seoul.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/28 11:42:53 by jaesjeon          #+#    #+#             */
-/*   Updated: 2021/12/01 21:10:50 by jaesjeon         ###   ########.fr       */
+/*   Updated: 2021/12/01 21:49:53 by jaesjeon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,4 +14,15 @@
 
 char *get_next_line(int fd)
 {
+	char		buffer[BUFFER_SIZE];
+	char		*result;
+	static char	*backup;
+
+	backup = (char *)malloc(1);
+	if (backup == NULL)
+		return (NULL);
+	if (*backup != '\0')
+	{
+
+	}
 }
