b884db67118b3b333bd90bf57f2624a9b43b8a74
jaesjeon <jaesjeon@student.42seoul.kr>
2021-12-18 17:59:09 +0900

    gnl_폴더 정리, 이전 제출 파일 클론 받아서 정리 필요

---
+++ Linked_list/gnl_tester.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+echo -e "\033[32;1m"NORM"\033[m"
+read $a
+norminette ./*.c ./*.h
+echo -e "\033[32;1m"gnlTester"\033[m"
+read $a
+git clone https://github.com/Tripouille/gnlTester.git
+cd gnlTester/
+make a
+cd ..
+echo -e "\033[32;1m"gnl_difflen_multi_fd_Tester"\033[m"
+read $a
+git clone git@github.com:kohkubo/gnl_tester.git
+cd gnl_tester/
+zsh test.sh
+cd ..
+rm -rf gnlTester/
+rm -rf gnl_tester/
+echo -e "\033[32;1m"DONE"\033[m"
